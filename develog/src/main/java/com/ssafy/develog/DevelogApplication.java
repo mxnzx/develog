42968a3c8e948975a45821f18da0a1b9c9384da6
류민지 <i0692631@naver.com>
2023-10-24 13:55:15 +0900

    Init: Backend 프로젝트 생성
    Former-commit-id: 6f6f37e059b06d63dc275b19e82736782dd69231

---
+++ develog/src/main/java/com/ssafy/develog/DevelogApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.develog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelogApplication.class, args);
+	}
+
+}
---
+++ develog/src/test/java/com/ssafy/develog/DevelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.develog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
