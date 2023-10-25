737aa70ed6fc4a33f975d97835df6f33d599163d
wldbs8241 <wldbs8241@naver.com>
2023-10-25 10:42:23 +0900

    Test: develog 파비콘 변경 테스트
    Former-commit-id: 6691f646e34495312d7f59a6f86a20c14137b828

--- frontend/develog/src/components/Resume/SideRegion/SideRegion.tsx
+++ frontend/develog/src/components/Resume/SideRegion/SideRegion.tsx
@@ -1,3 +1,18 @@
 import React from "react";
+import { backgroundWhite } from "style/Color";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${backgroundWhite};
+  /* position: fixed; */
+  width: 80rem;
+  height: 40rem;
+  border-radius: 2.5rem;
+`;`;
 // 사이드 탭 부분
+
+function SideRegion() {
+  <Container></Container>;
+}
+
+export default SideRegion;
