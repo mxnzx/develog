888757b04d8e40556128b39a0ae985d904aeff57
김선진 <tjswls8062@skuniv.ac.kr>
2023-10-26 13:12:36 +0900

    Feat: 범과 겹치는 코드 위한 커밋2
    Former-commit-id: a09b499c9dd441b563aca5153bdab7d85e60e034

--- Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7f040cf7f5b814c5cd67af628c04a9e445d2548f3a846948b12e541790151a3
-size 1760
+oid sha256:0e7e6eaef3bfdb9314d5fdde5716fea4a0ccade5f3043bb0bb9931ee91533802
+size 1768
--- Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a9d024e3a561d91bda871152250ccf922c9d407492a5426c96a79b25dbad2a5
-size 3841
+oid sha256:62543549845ca508bfb75e581dd9e2cc419f14bddd7f33c216d34a5c6c49d66a
+size 3851
