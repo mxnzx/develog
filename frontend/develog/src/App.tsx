7971af0e3c7fb1057e32c17815d2ead8bd1dc2dc
Dongmin <dl_skflsalfm@naver.com>
2023-10-26 09:23:34 +0900

    Feat: 면접 라우터 기본 설정 S09P31B210-135
    Former-commit-id: d8527dd37f88ce03348c12d771c6ac47b1844b3e

--- frontend/develog/src/App.tsx
+++ frontend/develog/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d341ea7c29e842d6899f7d3e141524b785ff80f5d1821d81203ee2a0ccda22
-size 1264
+oid sha256:641b0b37e066a35a9521d0c28565202a0082531ac4b19c4d04cf46953ecadffb
+size 1271
