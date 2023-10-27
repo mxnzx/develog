c859697ebccf67fd75c49005cfb6966cf9cf0058
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-27 11:12:17 +0900

    Refactor: Servlet Context Path 설정 및 관련 파일 uri 수정
    Former-commit-id: ce116e93b874743b0f410d1116e07188a4c27c76

--- Backend/develog/src/main/java/com/ssafy/develog/company/controller/CompanyController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/company/controller/CompanyController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e78d800f0396b5494d177678cfdfec73d0ffe0c5f3b76b49320324ae91cec7f
-size 1250
+oid sha256:75a1ceb24ea9495aa6dd5045e3999f5150a94f7534a5ceee720d803a79c5bf7f
+size 1246
--- Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee15bcec7f3fde3d5cbce7b035b70f281d59ae53c055f207eaf9cae46708f1e0
-size 2208
+oid sha256:e3230fa512e69b13c64c5b4c021aad1c947f20b8b3cc27ad2638ff567b49503d
+size 2204
