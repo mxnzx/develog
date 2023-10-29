41d3beb689fa90ecaa55f1345c7aca0153cc447b
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-30 02:03:18 +0900

    Feat: 포트폴리오 자격증 조회, 추가 및 수정 완료
    Former-commit-id: a497ab2860b15b0be1edec87b4ca353079b13183

---
+++ Backend/develog/src/main/java/com/ssafy/develog/user/domain/License.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9cd5006215aa6678344444ad309145c29ba7b81bfcba8bb425cdc56f0ee8ff32
+size 1005
--- Backend/develog/src/main/java/com/ssafy/develog/user/dto/response/ResponseLicenseDto.java
+++ Backend/develog/src/main/java/com/ssafy/develog/user/dto/response/ResponseLicenseDto.java
@@ -3,7 +3,15 @@ package com.ssafy.develog.user.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class ResponseLicenseDto {
+
+    private String title;
+    private String organization;
+    private String serialNum;
+    private LocalDateTime acquisitionDate;
+    private LocalDateTime expireDate;
 }
