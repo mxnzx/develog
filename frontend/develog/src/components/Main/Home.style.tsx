872c15644753bfd5b66af51211d6f820d8a5ec74
박소현 <park.sohyun0430@gmail.com>
2023-10-30 17:08:18 +0900

    [S09P31B210-208] Feat: 관심 있는 기업 component 구성 완료
    Former-commit-id: 20b7586a20819f35dbdf40efd663323458e88076

--- frontend/develog/src/components/Main/Home.style.tsx
+++ frontend/develog/src/components/Main/Home.style.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba91d63e4c91a95799838ebe7907aac8a806a44a5d9b8678cb14f9be41ec3c6c
+oid sha256:54b8b27b278c842255fa4a785eeb3ba718e843dddde468fa7e43b6949d78d10a
 size 515
--- frontend/develog/src/components/Main/HomeCompany.tsx
+++ frontend/develog/src/components/Main/HomeCompany.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import * as S from "./HomeCompany.style";
-const HomeCompany = () => {
-  return <S.CompanyContainer></S.CompanyContainer>;
-};
-
-export default HomeCompany;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ee2897a2ce1a71ae857ae53c706562a09a2ce44ea340cfaa934381b4dae3a2a
+size 1774
