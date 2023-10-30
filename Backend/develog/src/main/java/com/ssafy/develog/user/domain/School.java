255eff56e78ce486a837c3e2da08ea1d7cc17184
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-30 09:11:46 +0900

    WIP: 학력 조회 및 등록 위한 레포, School 클래스명 변경
    Former-commit-id: 2fd1e59dec832cb63efba0c63ed1a12ffade85c6

---
+++ Backend/develog/src/main/java/com/ssafy/develog/user/domain/School.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:032d3f6218b925941eeda7edbce01c8f83def96e1ef16f5970cc560e4a238ac2
+size 895
---
+++ Backend/develog/src/main/java/com/ssafy/develog/user/domain/SchoolRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.develog.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
