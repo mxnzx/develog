3e2d80d225c8cf0f635cf247cbd648656466a280
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-10-30 11:06:31 +0900

    Fix: 경로 및 code format 수정
    Former-commit-id: afda5926c8500a7b6460fe5f5f449b852c5768c9

--- frontend/develog/src/store/index.tsx
+++ frontend/develog/src/store/index.tsx
@@ -1,6 +1,6 @@
 // store.ts
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../store/reducers/user'; // 나중에 작성할 userSlice 파일을 가져옵니다.
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "store/reducers/user"; // 나중에 작성할 userSlice 파일을 가져옵니다.
 
 const store = configureStore({
   reducer: {
