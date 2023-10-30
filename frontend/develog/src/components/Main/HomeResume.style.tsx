d1ea8d4a9a894f2cbc381d03a6f96fd9a02e4693
박소현 <park.sohyun0430@gmail.com>
2023-10-30 17:52:20 +0900

    [S09P31B210-208] Resume container 구현 완료
    Former-commit-id: 541ccee7586db5918e4218000be13672a043bca9

--- frontend/develog/src/components/Main/HomeResume.style.tsx
+++ frontend/develog/src/components/Main/HomeResume.style.tsx
@@ -1,6 +1,3 @@
-import styled from "styled-components";
-import { Outline } from "./Home.style";
-
-export const ResumeContainer = styled(Outline)`
-  width: 50%;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9a2a84f250b3ecc7ec3a9dfcc270f5fa94ddca4a7772b0fd3b8fa493afdbc86
+size 1298
