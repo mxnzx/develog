d365931c1ee1614965d246311ba79abcee341661
wldbs8241 <wldbs8241@naver.com>
2023-10-30 17:44:46 +0900

    Feat: 자소서 메인 영역 헤더 구현 S09P31B210-236
    Former-commit-id: 23e43a4ea7058ab2d42038c5a24d0ab52291706d

---
+++ frontend/develog/src/components/Resume/MainRegion/MainRegion.style.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3341878853719d8d7b6b53036fc2965226f360edbd43eb7df8e346e6a55a7b09
+size 703
--- frontend/develog/src/components/Resume/SideRegion/SideRegion.tsx
+++ frontend/develog/src/components/Resume/SideRegion/SideRegion.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8328dfdb11a859d42aebff139586f9437cca4f5c5ef1a4b109b719813d4de126
-size 1800
+oid sha256:e9b9c15b647d4fcf9b81bec769465df1a53416c70e61b75a44ba43ba3b6fa2e4
+size 1806
