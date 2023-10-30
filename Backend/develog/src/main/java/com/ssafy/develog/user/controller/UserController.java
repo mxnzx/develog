85c68f8e2bbc5619fc894fe6276abd782e61e22e
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-30 17:41:25 +0900

    Feat: 회원 탈퇴 기능 구현 (S09P31B210-193)
    Former-commit-id: e34216b7df175c4a70407449b4880bf5d6075b98

--- Backend/develog/src/main/java/com/ssafy/develog/user/controller/UserController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37961acb96c46498172340fe334bfa8dd28b531e07b2e6539dbbeab9c256d4e7
-size 2105
+oid sha256:eab9aa5214681d1ee38d4f766d99bcc76e180a7a969d5e5a4ef34a587a887bb0
+size 2698
