99809654688592675160f7e594a09fbd35565d5e
김선진 <tjswls8062@skuniv.ac.kr>
2023-10-31 15:17:37 +0900

    Feat: 모의면접을 위한 질문 리스트 조회 기능 구현 리팩토링 이전
    Former-commit-id: 180805866362221df3a936b17b71f57dbb6dd89e

--- Backend/develog/src/main/java/com/ssafy/develog/interview/domain/InterviewRepository.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/domain/InterviewRepository.java
@@ -1,7 +1,3 @@
-package com.ssafy.develog.interview.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterviewRepository extends JpaRepository<Interview, Long> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9b0bb9f78536a4db0e09b227a8cbb0c25fb59c07252832c8e66dd28349b6d16
+size 609
--- Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:942cf446ef6ef91399ad44c3dd08ac2ac4596bd86d5b34e2a037bb1ee1599573
-size 14759
+oid sha256:23956dd3af91e5f8c61b689540d81c657ea4148b3a168c687e8c46a2bd0d0cd0
+size 15319
