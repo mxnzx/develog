bd92a2e94abb0681b376c92acc8bedc873e2d6b2
bmsnc <qjatjs98@gmail.com>
2023-10-31 16:42:48 +0900

    Feat: 자소서에 해당하는 지원정보 조회 API S09P31B210-207
    Former-commit-id: a10f171a8f984625574183cfc5deb1246ab5af5f

--- Backend/develog/src/main/java/com/ssafy/develog/resume/controller/ResumeController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/resume/controller/ResumeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3a12314c5181de951af23ed814ff1089782daacd5fd7b25ef13375210604ae0
-size 893
+oid sha256:9168f64c5bf7c5da610b33f49441715d03289c74a2d90c041562e05ecda30859
+size 878
--- Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f99d061a19bab8a35e2704b845ce53e4ff7713c75ce601115a002fe73e157e83
-size 1841
+oid sha256:52486715a2023b3a971aaed3130fe0b9910e73de8c0efeaae20ebf9621e29917
+size 2530
