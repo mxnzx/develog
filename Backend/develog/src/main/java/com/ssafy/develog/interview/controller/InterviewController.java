718e09ddfe390526f8d4e6d494c0b16a28da9032
김선진 <tjswls8062@skuniv.ac.kr>
2023-10-31 14:49:18 +0900

    Feat: 자소서와 무관한 사용자의 질문 생성 기능 구현 완료
    Former-commit-id: a9105f1eb4018bfd30b8369bed2680981dc18bbe

--- Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dc14e73675f83daf9731df3eff68d58a29812a1077011923b26f9eec0285bfe
-size 5842
+oid sha256:fddc3176c034090c386c8a6ab73ada672096cc212e18c8a9b13871852e870745
+size 6299
--- Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9915621fac474343cd9afafc33f11927634195cf4e882955841afe128a8349e
-size 14116
+oid sha256:942cf446ef6ef91399ad44c3dd08ac2ac4596bd86d5b34e2a037bb1ee1599573
+size 14759
