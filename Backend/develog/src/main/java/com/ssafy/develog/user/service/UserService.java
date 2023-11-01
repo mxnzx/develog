714adba672fdc2a3c7d988ba6cee11be6d245bd7
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-01 22:33:26 +0900

    Feat: 메인페이지 불러올 때 null 처리 완료 API 끝!!!!
    Former-commit-id: 15040f86de6da27b6b86ebe59b893b1c8b0602dd

--- Backend/develog/src/main/java/com/ssafy/develog/user/service/UserService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6305d1ab9c94eb2fa9ddbf6d410e49d3b8e2a80046d78f387407eaa5a86b6dbd
-size 4519
+oid sha256:8b15bd34bfd7b415e9565fc67aa34d2eb587d2665da5c56a21231608ef9cefc6
+size 4807
