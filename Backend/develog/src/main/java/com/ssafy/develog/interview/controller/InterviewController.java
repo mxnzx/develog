9783e0b7c982ad4e18fe146bb5ceb59763dd9b87
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-02 09:36:09 +0900

    WIP: 모의 면접 삭제 기능
    Former-commit-id: 9e43fa134d231894401403f7c5b10a005652dbde

--- Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/controller/InterviewController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a5cd8e689382a9f87df12262f7561f5613545962e7e3d0e15e4b963e61ec1d8
-size 6289
+oid sha256:ce8dab0c25ec834e5bbf24b4c038a763e64ea1955e4df9d994fdf9153ade6031
+size 6712
---
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/service/RecordService.java
@@ -0,0 +1,17 @@
+package com.ssafy.develog.interview.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RecordService {
+
+    public void deleteExamFile(Long recordId) {
+    }
+}
