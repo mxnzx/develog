d0de8dc8e9536a4c3578d46f8061c8147b903cc8
Dongmin <dl_skflsalfm@naver.com>
2023-11-02 17:12:16 +0900

    Fix: 6차시도
    Former-commit-id: fc9e0add044b07c2032af2f3fb0c908aa1f5de4f

--- frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
+++ frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e227fe0839c7c79f91b91612eae206e2adbf38a93aa5d493050f7cd1c74b139d
-size 1570
+oid sha256:bd5ff7dfa4d14ea88db6f0c392b78fa448b905c7f089c840213932202a13cf16
+size 1568
