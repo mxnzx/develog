2bcff7052b74467cd226f770a1057612aa160c58
박소현 <park.sohyun0430@gmail.com>
2023-11-02 16:38:39 +0900

    [S09P31B210-230] Feat: 스크립트 작성 페이지 녹음전 왼쪽 컴포넌트 완성
    Former-commit-id: a9d195964717bd70422b887b3c196817b44205b2

---
+++ frontend/develog/src/components/Interview/Script/Script.style.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f7a8430264e7eb8bc3666d685cd1927dffde28c01bdd0a2186391673a1d1a30
+size 1840
--- frontend/develog/src/components/Interview/Script/Script.tsx
+++ frontend/develog/src/components/Interview/Script/Script.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50d4df507989485984c66b41c33bbca8298a4cdbcf4d28e76113e021a07a97e7
-size 866
+oid sha256:14e247955c6462182308b2a5ef943a4ce9b387841629adbd02474c1cdc0196a4
+size 4394
