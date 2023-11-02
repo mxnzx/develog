b5d2c408232b91edbb95194ab169e65554654fef
Dongmin <dl_skflsalfm@naver.com>
2023-11-02 16:06:46 +0900

    Feat: URL 수정
    Former-commit-id: 21d4a32ab90db8f31136534d85c97aec921d4990

--- frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
+++ frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:231004adc59c6b59b75667fd4fcaa97c4e9ad2d33825aead6b178c0592c0581b
-size 1656
+oid sha256:2f5271cc3862220b0eb99d9ed8e27119646d343277704638876dcf93265b8e99
+size 1651
