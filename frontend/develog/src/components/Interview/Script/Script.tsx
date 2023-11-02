f8b0bdc2eda4b7ef89363abddbf5795d7ef5c0e1
박소현 <park.sohyun0430@gmail.com>
2023-11-02 17:43:27 +0900

    [S09P31B210-230] Feat: 꼬리질문 모달 생성
    Former-commit-id: 30b054b40d2034a7447f7d8bc50c805e4a594c7c

--- frontend/develog/src/components/Interview/Script/Script.tsx
+++ frontend/develog/src/components/Interview/Script/Script.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf15196d56a323f107de081e864f2ef7705934f7ab8462f9cbedea6d93fa4673
-size 5329
+oid sha256:8b292efc8277a2a925176cb18b6289f15bf293fe3a71673eaf828d712f6b8e72
+size 9905
