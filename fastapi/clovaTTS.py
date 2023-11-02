ab6d3156b7f7e9c00e09e09f02c80a004420ab27
Dongmin <dl_skflsalfm@naver.com>
2023-11-02 15:47:07 +0900

    Fix: f{} 문자열 따옴표해결
    Former-commit-id: 00a6b64854365a0b0316b00f7ce82b2c1ab18d0a

--- fastapi/clovaTTS.py
+++ fastapi/clovaTTS.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f3e9be485512cb828f5be74e433c20e75bded74c61dfdbcfaf91cf71b5f0e00
-size 1458
+oid sha256:e31619f210830ba18e94fefe80861260585c3ff9258274a4522750aab00358b7
+size 1463
