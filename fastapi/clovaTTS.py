41bca0afa133e4208f66dfc9ef8be310393de350
Dongmin <dl_skflsalfm@naver.com>
2023-11-02 16:33:34 +0900

    Fix: 실행 취소
    Former-commit-id: 3f824c122f2b981a44142dcdec311e981ee7c26b

--- fastapi/clovaTTS.py
+++ fastapi/clovaTTS.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a6341524a1ca293688ad5552804a102e15c82428fedb7927163caebc0b7409
-size 1517
+oid sha256:a948a2d0d25243bcc06cf9e528b536e1ee9d6e8c9dc8be1f24e77229b0befea0
+size 1519
