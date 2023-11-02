6f66b529ec3c35a15894a82137d5966e02fce371
bmsnc <qjatjs98@gmail.com>
2023-11-02 16:40:37 +0900

    Feat: 자소서 API 1차 구현 끝 ? 시간 설정 해야함 ㅋ S09P31B210-225
    Former-commit-id: 150925c5d78a3e0726d2c336c336750c22750ae7

--- Backend/develog/src/main/java/com/ssafy/develog/resume/controller/ResumeController.java
+++ Backend/develog/src/main/java/com/ssafy/develog/resume/controller/ResumeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33ec6d21f11bca21eae0bc25f633fa388c98e55377c2d140ccb42e6c9f67ec7b
-size 2375
+oid sha256:b203c5e599fe7ba0b835eaecc61583efb3168470f9a5b624b7322fb23c8af89f
+size 2540
