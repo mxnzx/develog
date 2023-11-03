3c945d922f5d3ebf05e6d4712560e59e7e3697ac
Dongmin <dl_skflsalfm@naver.com>
2023-11-03 13:40:25 +0900

    Fix: TTS 시도
    Former-commit-id: 8ccb564cfe4d5c4bf7121a32c3b127b02a589eaa

--- frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
+++ frontend/develog/src/components/Interview/Practice/Audio/InterviewTTS.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9758f712df4f9945bb0b4558c59ca73f58b6232b7d75ed291d694697aef89dca
-size 3168
+oid sha256:24a72614347ec31c922364104f7fb24976fc4d488df58a70a93348f6af4a27fd
+size 3176
