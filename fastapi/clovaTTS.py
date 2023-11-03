79c10b2fe60c1822ebad29a7a10dcebf6041e679
Dongmin <dl_skflsalfm@naver.com>
2023-11-03 12:56:54 +0900

    Feat: 경로 2개로 나눔
    Former-commit-id: 6e73b7b4ca1dc9902edc5062991c1eabce6bf473

--- fastapi/clovaTTS.py
+++ fastapi/clovaTTS.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc011205df5a62987215563358a8ce43912b21565a48fcee7e8e41a6775586eb
-size 1626
+oid sha256:83a8c7296feeecec96c81cb46a311e3bb47effcbe62644d4bee92194a90fda7d
+size 1623
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ec392a7804260a86ba6b94c4fe08ae6ea99f6bf75a217ae565b818bb693bb5c
-size 1375
+oid sha256:710c9ba3a815b4611959399ee1821c484924bd5db781cf398c879cda82a66c21
+size 1387
