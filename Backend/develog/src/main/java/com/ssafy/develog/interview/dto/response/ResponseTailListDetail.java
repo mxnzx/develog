91e248f4c9fb585afca5123f50477569ad747e3b
bmsnc <qjatjs98@gmail.com>
2023-11-03 16:48:10 +0900

    Feat: 꼬리질문 상세 조회 API S09P31B210-226
    Former-commit-id: a355fecfa4931347cb8e377d76ced4b09c0abb15

--- Backend/develog/src/main/java/com/ssafy/develog/interview/dto/response/ResponseTailListDetail.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:51a417d8913e438e5e9c987e357a6bd05241e6595921142c58b4b2da24d0d993
-size 1259
--- Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/service/InterviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b7f6569b27a6a8c08dca83a0154bbbad7a6020bd20bec0c175193422bd5564c
-size 15144
+oid sha256:d103ea2e39d8d40cefa853ec75c6830b46133762539b420d174a28c12ba80140
+size 15877
