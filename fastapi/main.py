91a9ac05eb65ba6028a5c3b82e1265c5e446112a
Dongmin <dl_skflsalfm@naver.com>
2023-11-03 11:00:32 +0900

    Fix: 잘못된 경로 수정 S09P31B210-220
    Former-commit-id: 23256d2af484094091db3265b740a7f131e6b16e

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1375ad26d0ee7a3a35839df37ec2a767d0f784922f6ba6fa8c1582bff933abee
-size 1122
+oid sha256:780b705c868565a5281a60a46833649c6b3bcc62a4c8c2f6731c27cb1da48b71
+size 1109
