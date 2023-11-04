6c27cfb8620c66202c13f3aaa77eaaff222e7afe
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-11-05 02:13:41 +0900

    [S09P31B210-230] Feat: 꼬리 질문 스크립트 페이지 구현 - 이전 작성한 스크립트 redux에서 어떻게 관리할건지 생각하기
    Former-commit-id: 4fcf93142df42d00f7135f8694fd25c53d0a4269

--- frontend/develog/src/components/Interview/Script/Script.tsx
+++ frontend/develog/src/components/Interview/Script/Script.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0951b54f82e8cf2597cdcd9cd677a8ef75ced755ecfabd05fba2914587a7bf0a
-size 7499
+oid sha256:40259143b4f49b35f6439807b5c7d0f73b065fbfd0a6eb0f274ff404eaecc516
+size 7986
