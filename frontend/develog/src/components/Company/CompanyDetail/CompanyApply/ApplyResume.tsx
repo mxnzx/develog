064ab6344a0e6151186cfec9c6c7e04f64f1252b
wldbs8241 <wldbs8241@naver.com>
2023-11-06 13:39:03 +0900

    Feat: 기업상세 페이지 퍼블리싱 및 라우팅 처리 S09P31B210-303
    Former-commit-id: 95d41a234348c818eca41f1779b2b81b315e9e55

--- frontend/develog/src/components/Company/CompanyDetail/CompanyApply/ApplyResume.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyApply/ApplyResume.tsx
@@ -1,3 +1,3 @@
-import React from "react";
-
-// 기업조회 리스트의 자기소개서 컴포넌트
+version https://git-lfs.github.com/spec/v1
+oid sha256:4770ec4bede32213f8ff401dcf2a8ce5bff1ce59602095e7942c65864f3f58eb
+size 894
--- frontend/develog/src/components/Company/CompanyDetail/CompanyDetail.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f93ef2a88b40e5135bb46ac78424eb2147e87990f737582c787366ae6fb6b593
-size 6373
+oid sha256:21a48f9819d936be9f08dd4f1827bcac48a56991086437976020d41e5cd1bfad
+size 6147
