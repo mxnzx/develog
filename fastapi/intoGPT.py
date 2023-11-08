7eea0cc70668cad990b25d7ff159e52f4ca320db
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-08 20:52:27 +0900

    Fix: 꼬리 질문 생성 프롬프트 내용 변경
    Former-commit-id: a50cfbb903b6288df2f83a3241906a621056bf33

--- fastapi/intoGPT.py
+++ fastapi/intoGPT.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf3f43c5f3aeade1d5de08bd8efe1dcec11c37a2106cfd9e1576c7ac6f4e8233
-size 2272
+oid sha256:96f5cf732c85986b1cab38fc6a3c9006a39142dcebe5b2d566de9d7ae6176a36
+size 2241
