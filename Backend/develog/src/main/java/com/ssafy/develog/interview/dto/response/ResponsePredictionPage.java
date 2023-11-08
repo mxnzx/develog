a5301e431cc337362695d81aad6df45c0302cd69
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-08 21:51:48 +0900

    Hotfix: keyword 리스트 보낼 때 id 같이 보냄
    Former-commit-id: d3618aa3f3bf881db8823d6d3cbcd57750055d4c

--- Backend/develog/src/main/java/com/ssafy/develog/interview/dto/response/ResponsePredictionPage.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/dto/response/ResponsePredictionPage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0cc6d214fa19cd9ed6fafc9c76ddf5324f4775a99fa16e39984922214901850
-size 1788
+oid sha256:0cdad3022255b866f21b6f67071c5be21078129fed279f70aa961ac80e7c7e8d
+size 2265
