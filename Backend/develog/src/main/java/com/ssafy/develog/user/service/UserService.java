92d19b801569769d896d2f87b82e679980a3289f
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-09 13:14:16 +0900

    Hotfix: 관심기업 불러오기
    Former-commit-id: 8c39dc87a0312a94e11be2b84424503c1059ad41

--- Backend/develog/src/main/java/com/ssafy/develog/user/service/UserService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc951e405b9fd5c9403e9f4c165b1095e1e78ea87a90cbed8f983edcc284b343
-size 5189
+oid sha256:c07753fa05d3916d2282a6b8e340eea0c5946f0184531c508d1dbc542ad672e6
+size 5584
