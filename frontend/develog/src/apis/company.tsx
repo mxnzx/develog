32a58de44836fb6d9f2ba4f7ce992f27a32fb17a
wldbs8241 <wldbs8241@naver.com>
2023-11-10 15:55:18 +0900

    Feat: 기업 상세페이지 아이템 통신 S09P31B210-305
    Former-commit-id: fe58389d7de43e80a0668ec219e4080c7a3369a1

--- frontend/develog/src/apis/company.tsx
+++ frontend/develog/src/apis/company.tsx
@@ -1,14 +1,3 @@
-// 기업 페이지 API 작성
-import { privateApi, fastPrivateApi } from "apis";
-
-// 회원별 자소서 키워드 조회
-export const getCompanyInfo = async (companyId: number) => {
-  try {
-    console.log("기업정보 조회 try?", companyId);
-    const response = await privateApi.get(`/company/detail/header/${companyId}`);
-    console.log("기업정보 조회 결과", response);
-    return response.data.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:b33acb30c2ae40ee3b3f2e9b1396576b33a2fb937099823c8e4fec72a41bdb09
+size 870
--- frontend/develog/src/components/Company/CompanyDetail/CompanyHeader.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0dcd7b5323a6a859ecbe8d609d3a288aa9969f513d30c1944d78681596e45e6
-size 2291
+oid sha256:c1ad4b45722aa7a086a81886c8dc6bb5a4e1c9cd5bbc40ee97a63a4fa085d790
+size 2375
