0d31a4762873e4cd6b97ed0e2ca123900c696890
wldbs8241 <wldbs8241@naver.com>
2023-11-10 16:32:28 +0900

    Fix: 충돌 해결
    Former-commit-id: b3c1cba515199b1fc4c34868dd47de62a05cfc07

--- frontend/develog/src/apis/company.tsx
+++ frontend/develog/src/apis/company.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66493b82bd9478d775d764568276f99c6e2ceb446bf875dc57f808137a7c5cae
-size 1125
+oid sha256:e5f5b9d2afdbd2f1568e9f06d277edc8b62185dd271ace3abde491e0c38e717b
+size 1132
