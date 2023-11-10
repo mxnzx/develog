5d2095b4acbb9149b0ef830c8d8f3d507ca1dd2f
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-10 09:18:26 +0900

    WIP: 기업 상세 정보 페이지 통신 S09P31B210-301
    Former-commit-id: 5f8c6560bbe588c41e6f4aec45d34712b2a8e4bf

---
+++ frontend/develog/src/apis/company.tsx
@@ -0,0 +1,11 @@
+// 기업 정보 API
+// import { privateApi } from "apis";
+
+// export const getMainInfo = async () => {
+//   try {
+//     const response = await privateApi.get("/users/main");
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
