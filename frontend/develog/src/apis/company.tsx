9268926b3a4b768a0258d3fff5aa5dd82fa7d7e0
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-11 17:20:22 +0900

    Fix: 검색할때 userId 삭제
    Former-commit-id: 8365460f0d37539d4bbf4a86e80167b1bb3b46a2

--- frontend/develog/src/apis/company.tsx
+++ frontend/develog/src/apis/company.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc0886d307c84b9ba04974c6fe252d3cece72be76584163805d8d36e1ac5e554
-size 2307
+oid sha256:2ab8d103ac68695da8792c0c985a8ea30439401ceb4f5b880503bd93d27475ba
+size 2297
