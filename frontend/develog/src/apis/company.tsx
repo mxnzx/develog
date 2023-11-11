d334ea4226b822ba120424f67b02960ed34c8133
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-11 16:08:55 +0900

    Feat: 관심기업 지원 삭제 API 구현
    Former-commit-id: 13136a1e006920fc5ff0afff13a75d54c382398b

--- frontend/develog/src/apis/company.tsx
+++ frontend/develog/src/apis/company.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021377e61e8daf448b5d68b2024f5e7c3d7129210e1014ccac24d68f79f97f35
-size 1610
+oid sha256:cd6bd00483fdcee6d024607f3cd109e543463c894c2ef6c159a451d133c25200
+size 1859
--- frontend/develog/src/components/Company/CompanyDetail/CompanyDetail.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:516dd23bfaeca4c6690974f93184ac0df9750aad626c3e6304f9adccd8565c46
-size 7014
+oid sha256:a7bf26625cfc02fcc27b1f77dfbcef1ff7d577a2666be567e0893318c9ca4a1e
+size 7547
