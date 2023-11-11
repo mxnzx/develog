2020a094f5a269002a2f21fb2e6bea8fd405ec8b
yunie <wldbs8241@naver.com>
2023-11-11 14:02:16 +0900

    Feat: 관심기업 상세페이지 헤더 이미지 유무 로고 구현 09P31B210-307
    Former-commit-id: b591815e677ab7b790ae487fffe8a97149b6b701

--- frontend/develog/src/components/Company/CompanyDetail/CompanyHeader.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1ad4b45722aa7a086a81886c8dc6bb5a4e1c9cd5bbc40ee97a63a4fa085d790
-size 2375
+oid sha256:4e8cc0657027b3ed02a56ce1e85e563faa67e12d4c4d39331dbc0f06df7f5ea6
+size 2957
