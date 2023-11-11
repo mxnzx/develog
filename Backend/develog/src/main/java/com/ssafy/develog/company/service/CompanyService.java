72f90602bda5f79c578d0e27acf4dc00d221aaa6
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-11 17:04:01 +0900

    Fix: 관심기업 등록할 때 companyInfo에 없으면 저장하기(로고 url은 null임)
    Former-commit-id: 06e259f9b052ba71c4992b44fedee43f45fbba34

--- Backend/develog/src/main/java/com/ssafy/develog/company/service/CompanyService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/company/service/CompanyService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f914377351a0f867b79dae090abbeabbdef960bcd3cdb09c21b276e83b28b3d
-size 7443
+oid sha256:90f810d42a8dafb567c346445a9a4a8793feb3679c2a6373a19d96e2ad929cee
+size 7642
