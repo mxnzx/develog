e757b36c0f0fb056c31b2b983719882872951400
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-12 19:10:21 +0900

    Feat: 자소서 처음 갈때 resume 데이터 생성+ resumeDetail 함께 생성
    Former-commit-id: ab9c5d1bbac8ae4be3df8085935c2967dc65da3a

--- frontend/develog/src/components/Company/CompanyDetail/CompanyApply/ApplyResume.tsx
+++ frontend/develog/src/components/Company/CompanyDetail/CompanyApply/ApplyResume.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:422bc00093ae4d0dabdaaeac5fdedf75809f75e76b0e0b1ca185f045b5debfeb
-size 3291
+oid sha256:4a91e9175a0461b4fd391e05dc68547fa6af1abcb965a81fbcdd33a6627f76bb
+size 3407
