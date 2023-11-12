1413f7f1b9a0a163864a01d6f162195c5ae8aa0b
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-12 16:36:07 +0900

    Hotfix: 자소서 로그 찍어봄
    Former-commit-id: 998914ca36c51bb0d0d2a8d90c56723c10366de5

--- Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25291e18e9a99a1050c38fb013788eb98dceb47494275dfef615de14ea05678b
-size 7696
+oid sha256:1cca9054421fa5fad5f1e25c5c2d8a881b6eefe77dbf48a9766c21feef629b38
+size 7928
