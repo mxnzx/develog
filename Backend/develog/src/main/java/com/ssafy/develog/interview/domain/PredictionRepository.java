57298567ae0146bf877a92e0e57eed311dc70a0b
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-13 10:34:43 +0900

    Hotfix: 히스토리쪽에 get해올때 distinct 빠진거 두개 붙임. 되라!!!!
    Former-commit-id: bec08032b387fc49a6d46faab0e7181da752817b

--- Backend/develog/src/main/java/com/ssafy/develog/interview/domain/PredictionRepository.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/domain/PredictionRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5955742f460665dd316ba3474725ce2a6c220b5af83692695247e26da47a5cd4
-size 1667
+oid sha256:1dbf7fb2a1944ea7fd7c92f96bac54b8183a98e06702158d22b5095a8f9ace3c
+size 1685
