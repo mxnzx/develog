59d71a935e14662e1cdb23bcfa1b271f12449afe
yunie <wldbs8241@naver.com>
2023-11-14 17:52:02 +0900

    Style: 마이페이지 저장 버튼 수정
    Former-commit-id: 0ae7224114df2633b1a4b343847394786788eec5

--- frontend/develog/src/components/Info/Info.style.tsx
+++ frontend/develog/src/components/Info/Info.style.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84fa400ede0e4067d49f05c30f6681d49192fff3a5b511389e3016b479019f50
+oid sha256:577547e6dcc6fbe687da1897d51fa178ac9f2e7c8a526a72cb3f79d1036e4329
 size 3195
