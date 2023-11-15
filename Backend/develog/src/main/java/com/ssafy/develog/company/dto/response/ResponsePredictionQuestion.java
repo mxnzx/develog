f05eeb59bb974f8ab690db06a491f0dee806691f
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-15 13:16:39 +0900

    Hotfix: 면접 준비 리스조회 응답값에 resumeDetailId 넣기
    Former-commit-id: 4629739d18ccd72d2eeac21736160d7518f56059

--- Backend/develog/src/main/java/com/ssafy/develog/company/dto/response/ResponsePredictionQuestion.java
+++ Backend/develog/src/main/java/com/ssafy/develog/company/dto/response/ResponsePredictionQuestion.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76b31f7835c9c081401972da51cc9e09134c9fb30f06f01a62b243b90b21edf8
-size 1578
+oid sha256:76abb1b0b4487c5bfbf28ab0805d36ad30088197202cfb29d680c0dc572d1525
+size 1740
