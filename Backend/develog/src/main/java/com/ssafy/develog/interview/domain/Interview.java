23fc63c29d01d913857f2f552b925bba8a7fbcf8
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-15 16:02:24 +0900

    Hotfix: 인터뷰 최초 등록되면 history 에도 업데이트 해주기
    Former-commit-id: 6e87aa0b24f927fcf3b8bd193fb5fb580955cb7b

--- Backend/develog/src/main/java/com/ssafy/develog/interview/domain/Interview.java
+++ Backend/develog/src/main/java/com/ssafy/develog/interview/domain/Interview.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9008ac4439bc1a73525afeee64cc0a721aa18d5da68ec027ebabdfe1592ad98
-size 1338
+oid sha256:52071d8290573ae94993aeeb4ab6eff9ff2bcfab12543048e419a24754598dea
+size 1386
