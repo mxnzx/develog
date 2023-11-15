79f91798eb0aef19381f70b534c53ebc4ed6cc74
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-15 10:06:34 +0900

    Hotfix: 자소서 등록 오류때문 카테고리 삭제하고  flush 추가
    Former-commit-id: b357f46bac0c6c70c0bb806df770e54dd7056b8c

--- Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
+++ Backend/develog/src/main/java/com/ssafy/develog/resume/service/ResumeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecb9729b61c2c96e78dc9c1bbe0f95aa9301e2c87eb8f6faee9c82f023244ffa
-size 8084
+oid sha256:ccd5f4de09455d9437b75e1a7b132ccdd3ffe0907837b71f1a242d86228b3b62
+size 8097
