a183774dc2897349a3ac406c793697bbdda7f1b6
yunie <wldbs8241@naver.com>
2023-11-16 14:12:16 +0900

    Fix: 자소서 페이지 임시저장 처리
    Former-commit-id: b16911d2b6f0b3dcb5f8daeeba730cd3f3d09bf7

--- frontend/develog/src/components/Resume/MainRegion/MainRegion.style.tsx
+++ frontend/develog/src/components/Resume/MainRegion/MainRegion.style.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b138ba2063a7afd492eb196132e83d74185996e8c383973fa8ef04f48c06d8fa
-size 5686
+oid sha256:5b763974d44e4d571b05087e45e1e6912412df0f4635800fcdc5a03e42d70c98
+size 5707
