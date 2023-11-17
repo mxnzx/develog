b308ae0dafbb770c7925f92595db2c5e79988c36
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-11-17 12:29:28 +0900

    Fix: 소수점 분기 처리 로직 잘못된 거 수정
    Former-commit-id: ff0ac804fac92c8a61670b745dc2fcc0582734c5

--- frontend/develog/src/components/Interview/Script/SpeedResult.tsx
+++ frontend/develog/src/components/Interview/Script/SpeedResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3984d2b47b8af6c0957be02a3af2dff13b26945ac486f34e5a77cee398d99e2c
-size 1964
+oid sha256:26a0fb188d32109eb0b18f5c6b1ee2e8bcd378d0ac3102dc88a04a887166852e
+size 1962
